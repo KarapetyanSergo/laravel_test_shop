2a1196979705da679fe011910054bfa8ef545c55
KarapetyanSergo <sergokarapetyan.1811@gmail.com>
2022-04-05 09:15:29 -0700

    Create Order service pivot.

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,73 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Order;
-use App\Models\ProductAvailabilities;
-use App\Models\ProductUser;
-use App\Models\User;
+use App\Services\OrderService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
     public function create(): JsonResponse
     {
-        $user = Auth::user();
-        $carts = ProductUser::where('user_id', $user->id)->get();
+        $orderService = new OrderService();
+        $orderService->store();
 
-        $builder = $user->products();
-        $products = $builder->get();
-        $price = 0;
-        $productOrders = [];
-        $response = [];
-
-        foreach ($products as $product) {
-            $productId = $product->id;
-            $cartIndex = array_search($productId, array_column($carts->all(), 'product_id'));
-            $cartProductCount = $carts[$cartIndex]->count;
-            $cartProductSize = $carts[$cartIndex]->size;
-
-            $productAvailability = ProductAvailabilities::firstOrNew([
-                ['product_id', '=', $productId],
-                ['size', '=', $cartProductSize]
-            ]);
-
-            if ($productAvailability->count >= $cartProductCount) {
-                $productAvailability->count = $productAvailability->count - $cartProductCount;
-
-                $price = $price + $product->price;
-                $response['message'][] = 'You bought ' . $cartProductCount . ' products with id = ' . $productId;
-                var_dump('asd');
-                $productOrders[] = [
-                    'product_id' => $productId,
-                    'size' => $cartProductSize,
-                    'count' => $cartProductCount
-                ];
-            } else {
-                $response['message'][] = 'There are not so many products with id = ' . $productId;
-            }
-
-            $productAvailability->save();
-        }
-
-        if ($price > 0) {
-            $order = Order::create([
-                'user_id' => $user->id,
-                'price' => $price,
-            ]);
-
-            $orderId = $order->get()->first()->id;
-
-            for ($i=0; $i<count($productOrders); $i++) {
-                $productOrders[$i]['order_id'] = $orderId;
-            }
-
-            $order->products()->attach($productOrders);
-
-            $response['price'] = $price;
-        }
-
-        return response()->json($response);
+        return response()->json([
+            'message' => 'Success!'
+        ]);
     }
 }
---
+++ app/Services/OrderService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Order;
+use App\Models\ProductAvailabilities;
+use App\Models\ProductUser;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Collection;
+
+class OrderService
+{
+    public function store(): void
+    {
+        $user = Auth::user();
+        $carts = ProductUser::where('user_id', $user->id)->get();
+
+        $builder = $user->products();
+        $products = $builder->get();
+
+        $ordersData = $this->reductionNumberOfProducts($products, $carts);
+        $this->createOrder($user->id, $ordersData);
+    }
+
+    private function reductionNumberOfProducts(Collection $products, Collection $carts): array
+    {
+        $price = 0;
+        $productOrders = [];
+
+        foreach ($products as $product) {
+            $productId = $product->id;
+            $cartIndex = array_search($productId, array_column($carts->all(), 'product_id'));
+
+            $cartProductCount = $carts[$cartIndex]->count;
+            $cartProductSize = $carts[$cartIndex]->size;
+
+            $productAvailability = ProductAvailabilities::firstOrNew([
+                ['product_id', '=', $productId],
+                ['size', '=', $cartProductSize]
+            ]);
+
+            if ($productAvailability->count >= $cartProductCount) {
+                $productAvailability->count = $productAvailability->count - $cartProductCount;
+                $price = $price + $product->price;
+
+                $productOrders[] = [
+                    'product_id' => $productId,
+                    'size' => $cartProductSize,
+                    'count' => $cartProductCount
+                ];
+            } else {
+                // send notification to user
+            }
+
+            $productAvailability->save();
+        }
+
+        return [
+            'orders' => $productOrders,
+            'price' => $price
+        ];
+    }
+
+    private function createOrder(int $userId, array $ordersData): void
+    {
+        $orders = $ordersData['orders'];
+        $price = $ordersData['price'];
+
+        if ($orders) {
+            $order = Order::create([
+                'user_id' => $userId,
+                'price' => $price,
+            ]);
+
+            $orderId = $order->get()->first()->id;
+
+            for ($i = 0; $i < count($orders); $i++) {
+                $orders[$i]['order_id'] = $orderId;
+            }
+
+            $order->products()->attach($orders);
+            var_dump('asd');
+        }
+    }
+}
